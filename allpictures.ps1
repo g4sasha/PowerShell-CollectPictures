3eb4491da13961e261820d693b42befab91dd81a
Александр Гяч <as.gyach@gmail.com>
2023-08-18 13:40:54 +0300

    Create Script

---
+++ allpictures.ps1
@@ -0,0 +1,4 @@
+﻿$floderFoto = "C:\Users\agyac\Desktop\Pictures"
+New-Item -Path $floderFoto -ItemType Directory
+Get-ChildItem "C:\" -Include *.png,*.jpeg,*.jpg -Recurse | Copy-Item -Destination $floderFoto
+(Get-ChildItem $floderFoto ).Count | Out-File ".\Desktop\PicturesCount.txt"
