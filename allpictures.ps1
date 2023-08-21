1de35da1ef69bacd11878929b073b8d1cd3ec997
Александр Гяч <as.gyach@gmail.com>
2023-08-21 21:42:32 +0300

    Автоматизация поиска пути до рабочего стола
    Теперь путь подстраивается под имя пользователя и программу нужно только запустить, чтобы она работала. Кроме того была сделана полная инструкция в README

--- allpictures.ps1
+++ allpictures.ps1
@@ -1,4 +1,4 @@
-﻿$floderFoto = "C:\Users\agyac\Desktop\Pictures"
+﻿$floderFoto = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), "Pictures")
 New-Item -Path $floderFoto -ItemType Directory
 Get-ChildItem "C:\" -Include *.png,*.jpeg,*.jpg -Recurse | Copy-Item -Destination $floderFoto
-(Get-ChildItem $floderFoto ).Count | Out-File ".\Desktop\PicturesCount.txt"
+(Get-ChildItem $floderFoto).Count | Out-File "$floderFoto\PicturesCount.txt"
